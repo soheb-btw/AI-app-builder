36bc50db80a17d28c01f0c8b78301b7348b3705f
Soheb Attar <soheb.dev@gmail.com>
2025-01-22 16:50:38 +0530

    backend code clean up

--- be/src/index.ts
+++ be/src/index.ts
@@ -16,39 +16,14 @@ app.use(express.json())
 app.post("/template", async (req, res) => {
   const prompt = req.body.prompt;
 
-  // const response = await anthropic.messages.create({
-  //     messages: [{
-  //         role: 'user', content: prompt
-  //     }],
-  //     model: 'claude-3-5-sonnet-20241022',
-  //     max_tokens: 200,
-  //     system: "Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anything extra"
-  // })
-
-
   const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY || '');
   const model = genAI.getGenerativeModel({
     model: "gemini-1.5-flash",
     systemInstruction: `Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anything extra`,
   });
 
-  console.log('prompt', prompt);
-  // console.log('chat````````````````', JSON.stringify(chat.params?.history));
   let result = await model.generateContent(prompt);
-  console.log(result.response.text());
-  // const chat = model.startChat({
-  //   history: [
-  //     {
-  //       role: "user",
-  //       parts: [{ text: "Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anything extra" }],
-  //     },
-  //     {
-  //         role: "model",
-  //         parts: [{ text: "Sure I will return only a single word."}]
-  //     }
-  //   ],
-  // });
-
+ 
   const answer = result.response.text().split('\n')[0]; // react or node
   if (answer === "react") {
     res.json({
@@ -60,7 +35,7 @@ app.post("/template", async (req, res) => {
 
   if (answer === "node") {
     res.json({
-      prompts: [`Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${nodeBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`],
+      prompts: [],
       uiPrompts: [nodeBasePrompt]
     })
     return;
@@ -73,8 +48,6 @@ app.post("/template", async (req, res) => {
 
 app.post("/chat", async (req, res) => {
   const messages = req.body.messages;
-  console.log('messagesss', messages);
-  console.log('------------------------------' );
 
   const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY || '');
   const model = genAI.getGenerativeModel({
@@ -82,8 +55,6 @@ app.post("/chat", async (req, res) => {
     systemInstruction: getSystemPrompt(),
   });
   let result = await model.generateContent(JSON.stringify(messages));
-  console.log(result.response.text());
-  console.log('enddinggggggg');
   const response = result.response.text(); // react or node
   res.json({
     response: response
@@ -91,35 +62,3 @@ app.post("/chat", async (req, res) => {
 })
 
 app.listen(3000);
-
-async function run() {
-  console.log(process.env.GEMINI_API_KEY);
-  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY || '');
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-  const chat = model.startChat({
-    history: [
-      {
-        role: "user",
-        parts: [{ text: "Hello" }],
-      },
-      {
-        role: "model",
-        parts: [{ text: "Great to meet you. What would you like to know?" }],
-      },
-      {
-        role: "user",
-        parts: [{ text: 'I have 2 dogs in my house.' }]
-      },
-      {
-        role: "model",
-        parts: [{ text: 'Thats lovely!  What breeds are they?  Do they get along well?  Anything exciting happening in your canine household today?' }]
-      }
-    ],
-  });
-  let result = await chat.sendMessage("I have 2 dogs in my house.");
-  console.log(result.response.text());
-  
-}
-
-// run();
-
